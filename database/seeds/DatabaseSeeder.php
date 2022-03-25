96d6855ff4a31d60621848fb8a6d617385d394b2
madrid <abdoelhabiby97@gmail.com>
2022-03-25 10:26:52 +0200

    fixes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \Eloquent::unguard();
+        // \Eloquent::unguard();
 
 		//disable foreign key check for this connection before running seeders
 		DB::statement('SET FOREIGN_KEY_CHECKS=0;');
