82d24995b0e614fd54c2cb899e1049cb60164f71
madrid <abdoelhabiby97@gmail.com>
2022-03-25 08:41:51 +0200

    seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-           // $this->call(UserSeeder::class);
-           // $this->call(AdminSeeder::class);
+           $this->call(UserSeeder::class);
+           $this->call(AdminSeeder::class);
            $this->call(CategoriesSeeder::class);
            $this->call(ProductSeeder::class);
     }
