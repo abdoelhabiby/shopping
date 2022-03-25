6db8febb97e4bc3b59b119a839003d76c5b85efd
madrid <abdoelhabiby97@gmail.com>
2022-03-25 10:27:43 +0200

    fixes

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,14 +15,14 @@ class DatabaseSeeder extends Seeder
         // \Eloquent::unguard();
 
 		//disable foreign key check for this connection before running seeders
-		DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+		// DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
            $this->call(UserSeeder::class);
            $this->call(AdminSeeder::class);
            $this->call(CategoriesSeeder::class);
            $this->call(ProductSeeder::class);
 
-           DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+        //    DB::statement('SET FOREIGN_KEY_CHECKS=1;');
 
     }
 }
